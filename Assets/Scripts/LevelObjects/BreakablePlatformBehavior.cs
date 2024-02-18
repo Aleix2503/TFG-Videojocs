50e02249487648e783eafe8bcee43c8fec893c7c
Albert Ruiz Gismero <75435836+Albertalberto@users.noreply.github.com>
2024-02-18 23:03:40 +0100

    Breakable platform WIP

---
+++ Assets/Scripts/LevelObjects/BreakablePlatformBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatformBehavior : MonoBehaviour
+{
+    public float velocityToBreak = -15;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rb2D = collision.gameObject.GetComponent<Rigidbody2D>();
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            print(rb2D.velocity.y);
+            if (rb2D.velocity.y <= velocityToBreak) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
