8bd2836beecf29df54b2018f79bd2ffeea9644b6
Albert Ruiz Gismero <75435836+Albertalberto@users.noreply.github.com>
2024-02-22 10:50:51 +0100

    Camera player follow behavior

--- Assets/Scripts/Camera/CameraPlayerFollow.cs
+++ Assets/Scripts/Camera/CameraPlayerFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,16 @@ public class CameraPlayerFollow : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField] private Transform target;
     [SerializeField] private Rigidbody2D targetRB2D;
-    [SerializeField] private float smoothTime = 0.1f;
+    [SerializeField] private float targetZ = 10;
+    [SerializeField] private float smoothTimeX = 0.1f;
+    [SerializeField] private float smoothTimeY = 2f;
+    [SerializeField] private float smoothTimeYOutsideBoundsUp = 0.2f;
+    [SerializeField] private float smoothTimeYOutsideBoundsDown = 0.02f;
     [SerializeField] private float YOffset;
     [SerializeField] private float XOffset;
+
+    [SerializeField] private float YDownLimit = -1;
+    [SerializeField] private float YUpLimit = 3;
 
     [SerializeField] private Vector2 cameraBoundariesMin;
     [SerializeField] private Vector2 cameraBoundariesMax;
@@ -30,9 +38,32 @@ public class CameraPlayerFollow : MonoBehaviour
         if (target != null)
         {
             Vector3 targetPosition = CalculateTargetPosition();
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-            ApplyCameraBoundaries();
+            Vector3 finalPosition = transform.position;
+            finalPosition.x = Mathf.SmoothDamp(transform.position.x, targetPosition.x, ref velocity.x, smoothTimeX);
+
+            float relativePosition = target.position.y - transform.position.y;
+
+            if (relativePosition < YDownLimit)
+            {
+                finalPosition.y = Mathf.SmoothDamp(transform.position.y, targetPosition.y, ref velocity.y, smoothTimeYOutsideBoundsDown);
+            } else if (relativePosition > YUpLimit)
+            {
+                finalPosition.y = Mathf.SmoothDamp(transform.position.y, targetPosition.y, ref velocity.y, smoothTimeYOutsideBoundsUp);
+            } else
+            {
+                finalPosition.y = Mathf.SmoothDamp(transform.position.y, targetPosition.y, ref velocity.y, smoothTimeY);
+            }
+
+            //finalPosition = ApplyCameraBoundaries(finalPosition);
+
+            transform.position = finalPosition;
         }
+    }
+
+    private bool isTargetWithinYBounds()
+    {
+        float relativePosition = target.position.y - transform.position.y;
+        return relativePosition > YDownLimit && relativePosition < YUpLimit;
     }
 
     private Vector3 CalculateTargetPosition()
@@ -40,15 +71,14 @@ public class CameraPlayerFollow : MonoBehaviour
         float targetX = target.position.x + XOffset;
         float targetY = target.position.y + YOffset;
 
-        return new Vector3(targetX, targetY, transform.position.z);
+        return new Vector3(targetX, targetY, targetZ);
     }
 
-    private void ApplyCameraBoundaries()
+    private Vector3 ApplyCameraBoundaries(Vector2 position)
     {
-        Vector3 newPosition = transform.position;
-        newPosition.x = Mathf.Clamp(newPosition.x, cameraBoundariesMin.x, cameraBoundariesMax.x);
-        newPosition.y = Mathf.Clamp(newPosition.y, cameraBoundariesMin.y, cameraBoundariesMax.y);
-        transform.position = newPosition;
+        position.x = Mathf.Clamp(position.x, cameraBoundariesMin.x, cameraBoundariesMax.x);
+        position.y = Mathf.Clamp(position.y, cameraBoundariesMin.y, cameraBoundariesMax.y);
+        return position;
     }
 
     private void OnDrawGizmos()
