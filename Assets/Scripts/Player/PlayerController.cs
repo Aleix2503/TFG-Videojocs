17b6efc963d624fe51d1b2255508e86dfd72cc7c
Carlitusc48 <102961218+CarlesSilvestre@users.noreply.github.com>
2024-12-04 21:11:27 +0100

    Hide Cursor Recovered

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -52,6 +52,8 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         playerPhysics = GetComponent<PlayerPhysics>();
         stateMachine = new();
 
