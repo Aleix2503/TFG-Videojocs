72a5603647792be6e6fedc5a5d14e3d710fcaac3
Aleix2503 <103170523+Aleix2503@users.noreply.github.com>
2024-12-18 17:40:37 +0100

    jyf

--- Assets/Scripts/Enemies/States/Alert.cs
+++ Assets/Scripts/Enemies/States/Alert.cs
@@ -38,27 +38,18 @@ public class Alert : StateBehaviour
 
         if (type == ChasingType.Terrestrial)
             direction = new Vector2(direction.x, 0);
-        /*else if (type == ChasingType.Flying)
-            RotateTowardsPlayer();*/
 
         if (playerDetection.detectionType == PlayerDetection.DetectionType.Circle)
             Flip();
 
         // Aplicar movimiento al Rigidbody2D
         rb.velocity = direction * chasingSpeed;
-
-        /*// Ajustar la orientación del enemigo
-        if (direction.x != 0)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(direction.x), 1, 1); // Mirar hacia el jugador
-        }*/
     }
 
     private void Flip()
     {
         if (transform.position.x > player.position.x && spriteRenderer[0].flipX)
         {
-            Debug.Log("aqui");
             for (int i = 0; i < spriteRenderer.Length; i++)
                 spriteRenderer[i].flipX = false;
         }
@@ -67,20 +58,5 @@ public class Alert : StateBehaviour
             for (int i = 0; i < spriteRenderer.Length; i++)
                 spriteRenderer[i].flipX = true;
         }
-    }
-
-    void RotateTowardsPlayer()
-    {
-        // Calcular la dirección hacia el jugador
-        Vector2 direction = player.position - transform.position;
-
-        // Calcular el ángulo en radianes y convertirlo a grados
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // Crear una rotación en Z hacia el ángulo calculado
-        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
-
-        // Suavizar la rotación con Lerp o Slerp
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 }
