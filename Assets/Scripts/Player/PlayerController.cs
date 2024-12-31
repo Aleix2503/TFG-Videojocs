73d09709f4f24a5b63218f704ec1a827a9dfe7a5
Aleix2503 <103170523+Aleix2503@users.noreply.github.com>
2024-12-31 13:26:10 +0100

    cambios para build

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -61,8 +61,8 @@ public class PlayerController : MonoBehaviour
 
     public void Start()
     {
-        //Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         playerPhysics = GetComponent<PlayerPhysics>();
         stateMachine = ScriptableObject.CreateInstance<PlayerStateMachine>();
 
